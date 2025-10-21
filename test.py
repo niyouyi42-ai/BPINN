1935193442117f590876f5b5de5667fb536c75d3
niyouyi42 <niyouyi42@gmail.com>
2025-10-21 20:03:36 +0800

    implement NUTS

--- test.py
+++ test.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
 import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 import torch.distributions as dist
-
-
+import math
 
 def gradients(outputs, inputs, order=1):
     re_outputs = torch.real(outputs)
@@ -40,14 +38,14 @@ def G_terms(a,w,A,s,m,u):
     return G0,G1,G2
 
 
+
 class BNN(nn.Module):
     def __init__(self, layers):
         super().__init__()
         self.L = len(layers)-1
         self.layers = nn.ModuleList()
         for i in range(self.L):
-            layer = nn.Linear(layers[i], layers[i+1])
-            self.layers.append(layer)
+            self.layers.append(nn.Linear(layers[i], layers[i+1]))
 
     def forward_from_vector(self, vec, x):
         j = 0
@@ -66,10 +64,8 @@ class BNN(nn.Module):
         return y
 
     def num_params(self):
-        total = 0
-        for layer in self.layers:
-            total += layer.weight.numel() + layer.bias.numel()
-        return total
+        return sum(layer.weight.numel() + layer.bias.numel() for layer in self.layers)
+
 
 
 class BPINN:
@@ -89,107 +85,150 @@ class BPINN:
         w_real, w_imag, A_real, A_imag = vec[n_f + n_g:]
         f_out = self.f_net.forward_from_vector(var_f, x)
         g_out = self.g_net.forward_from_vector(var_g, u)
-
         f_complex = torch.view_as_complex(f_out)
         g_complex = torch.view_as_complex(g_out)
-
-        f_new = ((torch.exp(x.view(-1)-1)-1)*f_complex + 1).view(-1,1) 
-        g_new = ((torch.exp(u.view(-1)+1)-1)*g_complex + 1).view(-1,1) 
-
+        f_new = ((torch.exp(x.view(-1)-1)-1)*f_complex + 1).view(-1,1)
+        g_new = ((torch.exp(u.view(-1)+1)-1)*g_complex + 1).view(-1,1)
         w = torch.complex(w_real, w_imag)
         A = torch.complex(A_real, A_imag)
         return f_new, g_new, w, A
 
 
-class HMC:
-    def __init__(self, target_log_prob_fn, init_state, step_size=0.0001, num_leapfrog_steps=10, num_results=1500, num_burnin=1500):
+
+
+class NUTS:
+    def __init__(self, target_log_prob_fn, init_state, step_size=0.01, adapt_steps=1500, num_results=1500, max_tree_depth=10, target_accept=0.65):
         self.target_log_prob_fn = target_log_prob_fn
         self.current_state = init_state.clone().detach().requires_grad_(True)
         self.step_size = step_size
-        self.num_leapfrog_steps = num_leapfrog_steps
+        self.adapt_steps = adapt_steps
         self.num_results = num_results
-        self.num_burnin = num_burnin
+        self.max_tree_depth = max_tree_depth
+        self.target_accept = target_accept
 
-    def leapfrog(self, position, momentum):
-        position = position.clone().detach().requires_grad_(True)
-        momentum = momentum.clone().detach()
-        step_size = self.step_size
+    def grad_log_prob(self, theta):
+        theta = theta.clone().detach().requires_grad_(True)
+        logp = self.target_log_prob_fn(theta)
+        grad = torch.autograd.grad(logp, theta, grad_outputs=torch.ones_like(logp), create_graph=False)[0]
+        return logp.detach(), grad.detach()
 
-        potential = -self.target_log_prob_fn(position)
-        grad_potential = torch.autograd.grad(potential, position, grad_outputs=torch.ones_like(potential))[0]
-        momentum = momentum - 0.5 * step_size * grad_potential
+    def leapfrog(self, theta, r, grad, step_size):
+        r_half = r + 0.5 * step_size * grad
+        theta_new = theta + step_size * r_half
+        logp_new, grad_new = self.grad_log_prob(theta_new)
+        r_new = r_half + 0.5 * step_size * grad_new
+        return theta_new, r_new, grad_new, logp_new
 
-        for i in range(self.num_leapfrog_steps):
-            position = position + step_size * momentum
-            position.requires_grad_(True)
-            if i != self.num_leapfrog_steps - 1:
-                potential = -self.target_log_prob_fn(position)
-                grad_potential = torch.autograd.grad(potential, position, grad_outputs=torch.ones_like(potential))[0]
-                momentum = momentum - step_size * grad_potential
+    def kinetic(self, r):
+        return 0.5 * torch.sum(r ** 2)
 
-        potential = -self.target_log_prob_fn(position)
-        grad_potential = torch.autograd.grad(potential, position, grad_outputs=torch.ones_like(potential))[0]
-        momentum = momentum - 0.5 * step_size * grad_potential
-        momentum = -momentum
-        return position.detach(), momentum.detach()
+    def build_tree(self, theta, r, grad, log_u, v, j, step_size, logp0, delta_max=1000.0):
+        if j == 0:
+            theta_new, r_new, grad_new, logp_new = self.leapfrog(theta, r, grad, v * step_size)
+            joint = logp_new - self.kinetic(r_new)
+            n = int(log_u <= joint)
+            s = int(log_u < delta_max + joint)
+            alpha = min(1.0, math.exp(joint - (logp0 - self.kinetic(r))))
+            n_alpha = 1
+            return theta_new, r_new, grad_new, theta_new, r_new, grad_new, theta_new, n, s, alpha, n_alpha
+        else:
+            thetaminus, rminus, gradminus, thetaplus, rplus, gradplus, thetaprime, nprime, sprime, alpha, n_alpha = \
+                self.build_tree(theta, r, grad, log_u, v, j - 1, step_size, logp0)
+            if sprime == 1:
+                if v == -1:
+                    thetaminus, rminus, gradminus, _, _, _, thetaprime2, nprime2, sprime2, alpha2, n_alpha2 = \
+                        self.build_tree(thetaminus, rminus, gradminus, log_u, v, j - 1, step_size, logp0)
+                else:
+                    _, _, _, thetaplus, rplus, gradplus, thetaprime2, nprime2, sprime2, alpha2, n_alpha2 = \
+                        self.build_tree(thetaplus, rplus, gradplus, log_u, v, j - 1, step_size, logp0)
+                if nprime + nprime2 > 0 and torch.rand(()) < nprime2 / (nprime + nprime2):
+                    thetaprime = thetaprime2
+                nprime += nprime2
+                s1 = ((thetaplus - thetaminus) @ rminus) >= 0
+                s2 = ((thetaplus - thetaminus) @ rplus) >= 0
+                sprime = int(sprime and sprime2 and s1 and s2)
+                alpha += alpha2
+                n_alpha += n_alpha2
+            return thetaminus, rminus, gradminus, thetaplus, rplus, gradplus, thetaprime, nprime, sprime, alpha, n_alpha
 
     def run_chain(self):
         samples = []
-        losses = []
-        accept_count = 0
-        current_position = self.current_state.clone().detach().requires_grad_(True)
+        current_theta = self.current_state.clone().detach().requires_grad_(True)
+        logp, grad = self.grad_log_prob(current_theta)
+        step_size = self.step_size
+        mu = math.log(10 * step_size)
+        h_bar, log_eps_bar = 0, 0
+        gamma, t0, kappa = 0.05, 10.0, 0.75
+        accept_sum, total_count = 0, 0
 
-        for i in range(self.num_results + self.num_burnin):
-            current_momentum = torch.randn_like(current_position)
-            current_potential = -self.target_log_prob_fn(current_position)
-            current_kinetic = 0.5 * torch.sum(current_momentum ** 2)
-            current_hamiltonian = current_potential + current_kinetic
+        for t in range(1,self.num_results + self.adapt_steps):
+            m = t - 1
+            r0 = torch.randn_like(current_theta)
+            log_u = (logp - self.kinetic(r0)).item() - np.random.exponential(1.0)
 
-            proposed_position, proposed_momentum = self.leapfrog(current_position, current_momentum)
-            proposed_potential = -self.target_log_prob_fn(proposed_position)
-            proposed_kinetic = 0.5 * torch.sum(proposed_momentum ** 2)
-            proposed_hamiltonian = proposed_potential + proposed_kinetic
+            thetaminus, thetaplus = current_theta.clone(), current_theta.clone()
+            rminus, rplus = r0.clone(), r0.clone()
+            gradminus, gradplus = grad.clone(), grad.clone()
+            theta_prime = current_theta.clone()
+            n, s, j = 1, 1, 0
+            alpha_sum, n_alpha = 0.0, 0.0
 
+            while s == 1 and j < self.max_tree_depth:
+                v = 1 if torch.rand(()) < 0.5 else -1
+                if v == -1:
+                    thetaminus, rminus, gradminus, _, _, _, theta_prime2, nprime, sprime, alpha, n_alpha_ = \
+                        self.build_tree(thetaminus, rminus, gradminus, log_u, v, j, step_size, logp)
+                else:
+                    _, _, _, thetaplus, rplus, gradplus, theta_prime2, nprime, sprime, alpha, n_alpha_ = \
+                        self.build_tree(thetaplus, rplus, gradplus, log_u, v, j, step_size, logp)
+                if sprime == 1 and torch.rand(()) < nprime / max(n + nprime, 1):
+                    theta_prime = theta_prime2.clone()
+                n += nprime
+                s = sprime and ((thetaplus - thetaminus) @ rminus >= 0) and ((thetaplus - thetaminus) @ rplus >= 0)
+                alpha_sum += alpha
+                n_alpha += n_alpha_
+                j += 1
 
-            delta_H = current_hamiltonian - proposed_hamiltonian
-            acceptance_prob = torch.exp(delta_H).clamp(max=1.0)
+            accept_prob = alpha_sum / max(n_alpha, 1)
+            if torch.rand(()) < accept_prob:
+                current_theta = theta_prime.clone()
+                logp, grad = self.grad_log_prob(current_theta)
 
-
-            if torch.rand(1) < acceptance_prob:
-                current_position = proposed_position.clone().detach().requires_grad_(True)
-                if i >= self.num_burnin:
-                    accept_count += 1
+            if t < self.adapt_steps:
+                h_bar = (1 - 1/(t + t0)) * h_bar + (1/(t + t0)) * (self.target_accept - accept_prob)
+                log_step = mu - (math.sqrt(t)/gamma) * h_bar
+                step_size = math.exp(log_step)
+                log_eps_bar = (t ** (-kappa)) * log_step + (1 - t ** (-kappa)) * log_eps_bar
             else:
-                current_position = current_position.clone().detach().requires_grad_(True)
+                step_size = math.exp(log_eps_bar)
+                samples.append(current_theta.detach().numpy())
+                accept_sum += accept_prob
+                total_count += 1
 
-            if i >= self.num_burnin:
-                samples.append(current_position.detach().numpy())
+            if m % 1 == 0:
+                print(f"Iter {m}, step_size={step_size:.5f}, accept={accept_prob:.3f}")
 
-            #if i % 10 == 0:
-            #    print(f"迭代 {i}: w_real={current_position[-4]:.4f}, w_imag={current_position[-3]:.4f}")
+                f, g, w, A = bpinn.forward_from_vector(current_theta, x, u)
+                F0, F1, F2 = F_terms(a, w, A, bpinn.s, bpinn.m, x)
+                G0, G1, G2 = G_terms(a, w, A, bpinn.s, bpinn.m, u)
+                dfdt = gradients(f, x); d2fdt2 = gradients(dfdt, x)
+                dgdt = gradients(g, u); d2gdt2 = gradients(dgdt, u)
+                res_F = F2 * d2fdt2 + F1 * dfdt + F0 * f
+                res_G = G2 * d2gdt2 + G1 * dgdt + G0 * g
+                err_F = torch.mean(torch.abs(res_F)).item()
+                err_G = torch.mean(torch.abs(res_G)).item()
+                w_real, w_imag = w.real.item(), w.imag.item()
+                Leaver_real, Leaver_img = 0.85023, -0.14365
+                err_real = abs(100*(w_real - Leaver_real)/Leaver_real)                    
+                err_img = abs(100*(w_imag - Leaver_img)/Leaver_img)
+                avg_err = 0.5 * (err_real + err_img)
+                print(f"Iter {t:4d} | step_size={step_size:.5e} | accept={accept_prob:.3f} | err_F={err_F:.2e} | err_G={err_G:.2e} | avg_err={avg_err:.2f}%")
 
-            Leaver_real = 0.85023
-            Leaver_img = -0.14365
-            w_real = current_position[-4].detach().numpy()
-            w_img = current_position[-3].detach().numpy()
-            error_real = 100*(w_real - Leaver_real)/Leaver_real
-            error_img = 100*(w_img - Leaver_img)/Leaver_img
-            average_error = (np.abs(error_real) + np.abs(error_img))/2
-            losses.append(average_error)
+        acceptance_rate = accept_sum / max(total_count, 1)
+        return np.array(samples), acceptance_rate
 
-            if i % 10 == 0:
-                print(f"迭代 {i}: w_real={current_position[-4]:.4f}, w_imag={current_position[-3]:.4f}, 平均误差={average_error:.5f}%")
 
-        plt.figure()
-        plt.plot(losses)
-        plt.xlabel("Iteration")
-        plt.ylabel("Average % Error")
-        plt.title("HMC Loss evolution")
-        plt.show()
 
-        acceptance_rate = accept_count / self.num_results
-        samples = np.array(samples)
-        return samples, acceptance_rate
 
 bpinn = BPINN(layers_f=[1,200,200,200,200,2], layers_g=[1,200,200,200,200,2])
 a = torch.tensor(0.4999)
@@ -200,48 +239,29 @@ r_plus = (1 + torch.sqrt(1-4*a**2)) / 2
 x = torch.linspace(0,1/r_plus,N_x).view(-1,1).requires_grad_(True)
 u = torch.linspace(-1,1,N_u).view(-1,1).requires_grad_(True)
 
-
 vec_params = torch.load("pinn_params.pt")
-init_vec = vec_params
-init_vec = init_vec.clone().detach().requires_grad_(True)
+init_vec = vec_params.clone().detach().requires_grad_(True)
 
 def log_post(vec):
     noise_pdef = 0.05
     noise_pdeg = 0.05
-    '''
-    noise_w = 0.05
-    noise_A = 0.05
-    prior_sigma = 1.0
-    '''
-
     f, g, w, A = bpinn.forward_from_vector(vec, x, u)
     F0,F1,F2 = F_terms(a,w,A,s,m,x)
     G0,G1,G2 = G_terms(a,w,A,s,m,u)
-
-    dfdt = gradients(f,x)
-    d2fdt2 = gradients(dfdt,x)
-    dgdt = gradients(g,u)
-    d2gdt2 = gradients(dgdt,u)
-
+    dfdt = gradients(f,x); d2fdt2 = gradients(dfdt,x)
+    dgdt = gradients(g,u); d2gdt2 = gradients(dgdt,u)
     res_F = F2*d2fdt2 + F1*dfdt + F0*f
     res_G = G2*d2gdt2 + G1*dgdt + G0*g
-
     lik_F = dist.Normal(0., noise_pdef).log_prob(res_F.real).sum() + dist.Normal(0., noise_pdef).log_prob(res_F.imag).sum()
     lik_G = dist.Normal(0., noise_pdeg).log_prob(res_G.real).sum() + dist.Normal(0., noise_pdeg).log_prob(res_G.imag).sum()
-    
-    '''
-    log_prior = dist.Normal(0, prior_sigma).log_prob(vec[:-4]).sum()
-    log_prior += dist.Normal(0, noise_w).log_prob(vec[-4]).sum()
-    log_prior += dist.Normal(0, noise_w).log_prob(vec[-3]).sum()
-    log_prior += dist.Normal(0, noise_A).log_prob(vec[-2]).sum()
-    log_prior += dist.Normal(0, noise_A).log_prob(vec[-1]).sum()
-    '''
+    return lik_F + lik_G
 
-    return lik_F + lik_G 
-
-hmc = HMC(target_log_prob_fn=log_post, init_state=init_vec)
-samples, acceptance_rate = hmc.run_chain()
+nuts = NUTS(target_log_prob_fn=log_post, init_state=init_vec)
+samples, acceptance_rate = nuts.run_chain()
 print("形状:", samples.shape, "接受率:", acceptance_rate)
+
+
+
 
 w_realsum = 0
 w_imagsum = 0
@@ -270,7 +290,7 @@ print_results_extra(w_real,w_imag)
 
 vec_last = torch.tensor(samples[-1], dtype=torch.float32, requires_grad=True)
 
-# 使用相同的 forward 流程计算 f, g, 残差
+
 f, g, w, A = bpinn.forward_from_vector(vec_last, x, u)
 F0, F1, F2 = F_terms(a, w, A, s, m, x)
 G0, G1, G2 = G_terms(a, w, A, s, m, u)
@@ -283,7 +303,7 @@ d2gdt2 = gradients(dgdt, u)
 res_F = F2 * d2fdt2 + F1 * dfdt + F0 * f
 res_G = G2 * d2gdt2 + G1 * dgdt + G0 * g
 
-# 计算误差指标：L2范数与平均绝对误差
+
 error_F = torch.mean(torch.abs(res_F))
 error_G = torch.mean(torch.abs(res_G))
 
@@ -296,7 +316,6 @@ print(f"  PDE G 方程平均绝对误差: {error_G.item():.4e}")
 
 
 
-# 提取w实部和虚部的采样序列
 w_real_series = [s[-4] for s in samples]
 w_imag_series = [s[-3] for s in samples]
 iterations = np.arange(len(samples))
